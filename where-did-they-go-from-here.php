17608bbe49320a71ff53401721801ad0ff655b63
Ajay D'Souza <me@ajaydsouza.com>
2014-04-21 18:41:56 +0100

    Updated v1.7

--- where-did-they-go-from-here.php
+++ where-did-they-go-from-here.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Where did they go from here
-Version:     1.6
+Version:     1.7
 Plugin URI:  http://ajaydsouza.com/wordpress/plugins/where-did-they-go-from-here/
-Description: Show "Readers who viewed this page, also viewed" links on your page. Much like Amazon.com's product pages. Based on the plugin by <a href="http://weblogtoolscollection.com">Mark Ghosh</a>. 
+Description: Show "Readers who viewed this page, also viewed" links on your page. Much like Amazon.com's product pages. Based on the plugin by Mark Ghosh. 
 Author:      Ajay D'Souza
 Author URI:  http://ajaydsouza.com/
 */
