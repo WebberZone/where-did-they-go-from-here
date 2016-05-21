746821b1271d2b7f76aa7e5983afee147ebe062a
Ajay DSouza <me@ajaydsouza.com>
2016-05-21 23:47:17 +0100

    Fixed bugs in saving metabox

--- admin/admin-metabox.php
+++ admin/admin-metabox.php
@@ -6,14 +6,13 @@
  * @subpackage	Admin
  */
 
-
 /**
  * Function to add meta box in Write screens.
  *
  * @since 1.7
- * @param text   $post_type
- * @param object $post
- * @return void
+ *
+ * @param text   $post_type Post type.
+ * @param object $post Post object.
  */
 function wherego_add_meta_box( $post_type, $post ) {
 
@@ -47,20 +46,22 @@ function wherego_call_meta_box() {
 
 ?>
 	<p>
-		<label for="wherego_post_ids"><?php _e( "Followed posts' IDs:", 'where-did-they-go-from-here' ); ?></label>
+		<label for="wherego_post_ids"><?php esc_html_e( "Followed posts' IDs:", 'where-did-they-go-from-here' ); ?></label>
 		<input type="text" id="wherego_post_ids" name="wherego_post_ids" value="<?php echo esc_attr( $value ) ?>" size="25" />
-		<em><?php _e( 'Enter a comma separated list of valid post/page IDs. Save this post to see the updated list below.', 'where-did-they-go-from-here' ); ?></em>
+		<em><?php esc_html_e( 'Enter a comma separated list of valid post/page IDs. Save this post to see the updated list below.', 'where-did-they-go-from-here' ); ?></em>
 	</p>
 	<?php if ( $results ) { ?>
 
-		<h3><?php _e( 'Followed posts:', 'where-did-they-go-from-here' ); ?></h3>
+		<h3><?php esc_html_e( 'Followed posts:', 'where-did-they-go-from-here' ); ?></h3>
 		<ol>
 		<?php
 		foreach ( $results as $result ) {
 			$title = get_the_title( $result );
-			echo '<li>';
-			echo '<a href="' . get_permalink( $result ) . '" target="_blank" title="' . $title . '" class="wherego_title">' . $title . '</a>'; // Add title if post thumbnail is to be displayed after
-			echo '</li>';
+		?>
+			<li>
+				<a href="<?php echo esc_attr( get_permalink( $result ) ); ?>" target="_blank" title="<?php echo esc_attr( $title ); ?>" class="wherego_title"><?php echo esc_attr( $title ); ?></a>
+			</li>
+		<?php
 		}
 		?>
 		</ol>
@@ -74,25 +75,30 @@ function wherego_call_meta_box() {
  * Function to save the meta box.
  *
  * @since 1.7
- * @param mixed $post_id
- * @return void
+ * @param mixed $post_id Post ID.
  */
 function wherego_save_meta_box( $post_id ) {
-	// Bail if we're doing an auto save
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) { return; }
+	// Bail if we're doing an auto save.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
 
-	// if our nonce isn't there, or we can't verify it, bail
-	if ( ! isset( $_POST['wherego_meta_box_nonce'] ) || ! wp_verify_nonce( $_POST['wherego_meta_box_nonce'], 'wherego_meta_box' ) ) { return; }
+	// If our nonce isn't there, or we can't verify it, bail.
+	if ( ! isset( $_POST['wherego_meta_box_nonce'] ) || ! wp_verify_nonce( sanitize_key( $_POST['wherego_meta_box_nonce'] ), 'wherego_meta_box' ) ) { // Input var okay.
+		return;
+	}
 
-	// if our current user can't edit this post, bail
-	if ( ! current_user_can( 'edit_post' ) ) { return; }
+	// If our current user can't edit this post, bail.
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
 
-	if ( isset( $_POST['wherego_post_ids'] ) & ! empty( $_POST['wherego_post_ids'] ) ) {
+	if ( isset( $_POST['wherego_post_ids'] ) && ! empty( $_POST['wherego_post_ids'] ) ) { // Input var okay.
 
-		$wherego_post_ids = array_map( 'intval', explode( ',', $_POST['wherego_post_ids'] ) );
+		$wherego_post_ids = array_map( 'intval', explode( ',', sanitize_text_field( wp_unslash( $_POST['wherego_post_ids'] ) ) ) ); // Input var okay.
 
 		foreach ( $wherego_post_ids as $key => $value ) {
-			if ( 'publish' != get_post_status( $value ) ) {
+			if ( 'publish' !== get_post_status( $value ) ) {
 				unset( $wherego_post_ids[ $key ] );
 			}
 		}
@@ -100,17 +106,8 @@ function wherego_save_meta_box( $post_id ) {
 		$wherego_post_ids = '';
 	}
 
-	$linkpostids = get_post_meta( $post_id, 'wheredidtheycomefrom', true );
-	if ( $linkpostids && '' != $linkpostids ) {
-		$gotmeta = true;
-	} else {
-		$gotmeta = false;
-	}
-
-	if ( $gotmeta && '' != $wherego_post_ids ) {
+	if ( '' !== $wherego_post_ids ) {
 		update_post_meta( $post_id, 'wheredidtheycomefrom', $wherego_post_ids );
-	} elseif ( ! $gotmeta && '' != $wherego_post_ids ) {
-		add_post_meta( $post_id, 'wheredidtheycomefrom', $wherego_post_ids );
 	} else {
 		delete_post_meta( $post_id, 'wheredidtheycomefrom' );
 	}
