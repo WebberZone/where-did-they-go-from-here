7a35cfea3cbc2525588b7347f7478fbc8bb32065
Ajay DSouza <me@ajaydsouza.com>
2016-05-21 23:46:57 +0100

    Formatting fix

--- admin/sidebar-view.php
+++ admin/sidebar-view.php
@@ -9,8 +9,8 @@
 ?>
 
 <div id="donatediv" class="postbox"><div class="handlediv" title="Click to toggle"><br /></div>
-  <h3 class='hndle'><span><?php _e( 'Support the development', 'where-did-they-go-from-here' ); ?></span></h3>
-  <div class="inside">
+	<h3 class='hndle'><span><?php _e( 'Support the development', 'where-did-they-go-from-here' ); ?></span></h3>
+	<div class="inside">
 	<div id="donate-form">
 		<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 		<input type="hidden" name="cmd" value="_xclick">
@@ -26,11 +26,11 @@
 		<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 		</form>
 	</div>
-  </div>
+	</div>
 </div>
 <div id="followdiv" class="postbox"><div class="handlediv" title="Click to toggle"><br /></div>
-  <h3 class='hndle'><span><?php _e( 'Follow me', 'where-did-they-go-from-here' ); ?></span></h3>
-  <div class="inside">
+	<h3 class='hndle'><span><?php _e( 'Follow me', 'where-did-they-go-from-here' ); ?></span></h3>
+	<div class="inside">
 	<div id="follow-us">
 		<div style="text-align:center">
 			<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fajaydsouzacom%2F&tabs&width=260&height=100&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId=458036114376706" width="260" height="100" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
@@ -39,11 +39,11 @@
 		<div style="text-align:center"><a href="https://twitter.com/ajaydsouza" class="twitter-follow-button" data-show-count="false" data-size="large" data-dnt="true">Follow @ajaydsouza</a>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div>
 	</div>
-  </div>
+	</div>
 </div>
 <div id="qlinksdiv" class="postbox"><div class="handlediv" title="Click to toggle"><br /></div>
-  <h3 class='hndle'><span><?php _e( 'Quick links', 'where-did-they-go-from-here' ); ?></span></h3>
-  <div class="inside">
+	<h3 class='hndle'><span><?php _e( 'Quick links', 'where-did-they-go-from-here' ); ?></span></h3>
+	<div class="inside">
 	<div id="quick-links">
 		<ul>
 			<li><a href="http://ajaydsouza.com/wordpress/plugins/where-did-they-go-from-here/"><?php _e( 'Where did they go from here? plugin page', 'where-did-they-go-from-here' ); ?></a></li>
@@ -55,5 +55,5 @@
 			<li><a href="http://ajaydsouza.com/"><?php _e( "Ajay's blog", 'where-did-they-go-from-here' ); ?></a></li>
 		</ul>
 	</div>
-  </div>
+	</div>
 </div>
