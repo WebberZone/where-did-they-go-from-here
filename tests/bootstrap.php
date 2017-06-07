9c92be1c8e546763ff1458bab3c266938248265b
Ajay DSouza <me@ajaydsouza.com>
2017-06-07 21:32:24 +0100

    echo location for bootstrap tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,6 +18,7 @@ require_once $_tests_dir . '/includes/functions.php';
  */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/where-did-they-go-from-here.php';
+	echo dirname( dirname( __FILE__ ) ) . '/where-did-they-go-from-here.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
