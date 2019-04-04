ae39933b214f46afe96ec11d070131ad662b6a68
Ajay DSouza <me@ajaydsouza.com>
2019-04-04 19:31:49 +0100

    Tagged v2.3.0 beta

--- where-did-they-go-from-here.php
+++ where-did-they-go-from-here.php
@@ -14,7 +14,7 @@
  * Plugin Name: WebberZone Followed Posts
  * Plugin URI:  https://webberzone.com/plugins/webberzone-followed-posts/
  * Description: The best way to display posts followed by users a.k.a. "Readers who viewed this page, also viewed" links
- * Version:     2.2.0
+ * Version:     2.3.0-beta1
  * Author:      Ajay D'Souza
  * Author URI:  https://webberzone.com
  * License:     GPL-2.0+
