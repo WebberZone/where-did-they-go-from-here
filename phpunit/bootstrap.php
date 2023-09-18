0cfb244438db4176c785dd14c03a4ab4b8c2ab67
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2023-09-18 21:47:59 +0100

    PHPUnit

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -1,10 +1,7 @@
 <?php
 /**
  * PHPUnit bootstrap file.
- *
- * @package Better_Search_Plugin
  */
-require_once dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) . '/.composer/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
@@ -27,19 +24,12 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/where-did-they-go-from-here.php';
+	require dirname( __DIR__ ) . '/where-did-they-go-from-here.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Include the PHPUnit Polyfills autoloader.
+require dirname( __DIR__ ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
-
-activate_plugin( 'where-did-they-go-from-here/where-did-they-go-from-here.php' );
-
-echo "Installing WebberZone Followed Posts...\n";
-
-global $wherego_settings, $current_user;
-
-wherego_activation_hook( true );
-
-$wherego_settings = wherego_get_settings();
